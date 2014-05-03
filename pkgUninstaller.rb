080497f0958a69ea57901823f0b08f99881106cf
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-05-03 18:40:53 +0900

    fix author name

--- pkgUninstaller.rb
+++ pkgUninstaller.rb
@@ -1,5 +1,5 @@
 # pkgUninstaller -OS X app uninstaller-
-#     Copyright (C) 2014  Nakamura Yusuke
+#     Copyright (C) 2014  Yusuke Nakamura
 
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
